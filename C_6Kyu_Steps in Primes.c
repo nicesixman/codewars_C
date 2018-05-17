e95509df037c338e5b4f78d517e687396c253e60
nicesixman <nicesixman@naver.com>
2018-05-18 01:18:15 +0900

    Update C_6Kyu_Steps in Primes.c

--- C_6Kyu_Steps in Primes.c
+++ C_6Kyu_Steps in Primes.c
@@ -5,19 +5,15 @@
 long long* step(int g, long long m, long long n)
 {
 	// ★ 진척상황
-	/*
-	1. int g / long long m / long long n은 모두 작동한다.
-	2. 단, 현재는 위 작동점을 기준으로 모든 소수를 출력한다.
-	3. 따라서 앞으로 해야할 작업은 (마지막 소수 - 마지막 전 소수 = g)인 케이스를 구별하는 것.
-	4. 위 작업들이 끝나면 마지막으로 else if (steps > g)의 케이스(null 경우)도 추가해줘야 한다.
-	*/
+	// 1. 마지막단계로 return을 2개 반환하여 codewars 사이트에서 계산되기만 하면 된다.
 
 
 	int isPrime = 0;
-	int Count, reminderCount, holdCount;
+	int Count, reminderCount;
+	int matrix[10000], matrixCount = 0, i;
+	// 동적 배열은 C++ 사용할 때 시도하고, 현재(C)는 코드 구조를 쉽게 읽기 위해 정적 배열로 선언.
 
 
-	// 우리가 필요한건 약수가 아니라 소수였다. [Prime = 소수]였음..
 	// 1은 소수가 아니므로 2부터 시작하여야하지만, step함수는 start of the search 수(m)가 있다.
 	for (Count=m; Count<=n; Count++)
 	{
@@ -29,8 +25,7 @@ long long* step(int g, long long m, long long n)
 			// 허나 아래 조건문 수식 하나만으로는 나머지가 0이되는 모든 경우가 계산된다.
 			if (Count % reminderCount == 0)
 				isPrime = isPrime + 1;
-			// 예외처리를 하여 연산속도 증가.
-			// ...를 하고 싶었으나 아래 수식을 추가하면 계산식이 안맞는다. 일단 skip.
+			// (예외처리를 하여 연산속도 증가를 고려했으나 아래 수식을 추가하면 계산식이 안맞는다. 일단 skip.)
 			/*
 			else
 				break;
@@ -38,13 +33,21 @@ long long* step(int g, long long m, long long n)
 		}
 
 		// 모든 경우가 계산되는 것의 해결 방법으로써
-		// for문이 마무리 된 후 isPrime == 2일 때만 출력하게끔 전환하면 된다.
+		// 안쪽 for문이 마무리 된 후 isPrime == 2일 때만 배열에 수를 저장하게끔 코드를 짜면 된다.
 		if (isPrime == 2)
-			printf("%d ", Count);
+		{
+			// printf("%d ", Count);		// 배열 미사용시 소수 전체출력.
+			matrix[matrixCount] = Count;
+			matrixCount = matrixCount + 1;
+			if (matrixCount >= 2 && g == (matrix[matrixCount - 1] - matrix[matrixCount - 2]))
+			{
+				printf("%d, %d", matrix[matrixCount - 2], matrix[matrixCount - 1]);
+				return 0;
+			}
+		}
 		// isPrime = 0으로 다시 초기화.
 		isPrime = 0;
 	}
-
 	return 0;
 }
 
