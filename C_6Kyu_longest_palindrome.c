0b0c1f93ae0d3679923c1b2fac586d94d845ea7d
nicesixman <nicesixman@naver.com>
2018-12-13 00:17:39 +0900

    C_6Kyu_longest_palindrome.c
    https://www.codewars.com/kata/54bb6f887e5a80180900046b/train/c
    2차원 배열로 옮겨서 저장하는 플로우까지 완성.
    단어가 들어간 1차 배열은 단어+공백 후 2차 배열 줄넘김, 단어가 들어가지 않은 1차 배열은 NULL('\0')로 저장된다. (초기값)

--- C_6Kyu_longest_palindrome.c
+++ C_6Kyu_longest_palindrome.c
@@ -6,14 +6,31 @@ int longest_palindrome(const char *s)
 {
 	// 포인터 매개변수는 함수 내부/외부에서 모두 사용 가능.
 	const char *copied = s;
+	char devided[1000][1000] = { '\0' };
+	int devided_queueX = 0, devided_queueY = 0;
+	int answer = 0;
 
 	printf("-----------------------------------\n");
 	printf("Called longest_palindrome function.\n");
-	printf("Original string: ");
+
+	// C언어는 string을 지원 안하므로 2차원 배열을 이용하여 char별로 저장.
 	for (int slen=0; slen<(int)strlen(s); slen++)
 	{
-		putchar(*copied++);
+		devided[devided_queueX][devided_queueY] = *copied++;
+		printf("(%d, %d): %c", devided_queueX, devided_queueY, devided[devided_queueX][devided_queueY]);
+		if (devided[devided_queueX][devided_queueY] == ' ')
+		{
+			printf("(%d, %d): %c\n", devided_queueX, devided_queueY, devided[devided_queueX][devided_queueY]);
+			devided_queueX++;
+			devided_queueY = 0;
+		}
+		devided_queueY++;
 	}
+	// test string의 끝에는 공백이 들어가지 않으므로, 임의로 넣어준다.
+	devided[devided_queueX][devided_queueY] = ' ';	
+	if (devided[5][5] == ' ')
+		printf("TESTING");
+
 	printf("\nReversed string: ");
 	for (int slen=strlen(s); slen>0; slen--)
 	{
