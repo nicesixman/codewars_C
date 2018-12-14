ad4766ee46626db99cc21aaf18a27c2fe10fa168
nicesixman <nicesixman@naver.com>
2018-12-15 01:20:03 +0900

    C_6Kyu_longest_palindrome.c
    https://www.codewars.com/kata/54bb6f887e5a80180900046b/train/c
    공백을 제외하고 2차원 배열의 첫 번째 y(x,0)부터 저장하는 형태로 바꿨음.
    분할된 단어의 길이를 구하는 형태까지 구현이 완료되어서 실질적으로 palindrome을 찾기 위한 코드만 구현하면 될 듯.

--- C_6Kyu_longest_palindrome.c
+++ C_6Kyu_longest_palindrome.c
@@ -8,36 +8,48 @@ int longest_palindrome(const char *s)
 	const char *copied = s;
 	char devided[1000][1000] = { '\0' };
 	int devided_queueX = 0, devided_queueY = 0;
-	int answer = 0;
+	int find_queueX = 0, find_queueY = 0;
+	int answer_queue = 0, answer = 0;
 
 	printf("-----------------------------------\n");
 	printf("Called longest_palindrome function.\n");
 
 	// C언어는 string을 지원 안하므로 2차원 배열을 이용하여 char별로 저장.
+	// 공백 개수를 포함한 char 개수만큼 for문 반복. 실제로는 공백은 빠진채로 저장됨.
 	for (int slen=0; slen<(int)strlen(s); slen++)
 	{
 		devided[devided_queueX][devided_queueY] = *copied++;
-		printf("(%d, %d): %c", devided_queueX, devided_queueY, devided[devided_queueX][devided_queueY]);
 		if (devided[devided_queueX][devided_queueY] == ' ')
 		{
-			printf("(%d, %d): %c\n", devided_queueX, devided_queueY, devided[devided_queueX][devided_queueY]);
 			devided_queueX++;
 			devided_queueY = 0;
+			devided[devided_queueX][devided_queueY] = *copied++;		// x,0 공백 제거용
 		}
 		devided_queueY++;
 	}
-	// test string의 끝에는 공백이 들어가지 않으므로, 임의로 넣어준다.
-	devided[devided_queueX][devided_queueY] = ' ';	
-	if (devided[5][5] == ' ')
-		printf("TESTING");
 
-	printf("\nReversed string: ");
-	for (int slen=strlen(s); slen>0; slen--)
+	// 실질적으로 palindrome을 찾는 부분.
+	for (int slen=0; slen<(int)strlen(s); slen++)
 	{
-		putchar(*--copied);
+		printf("%c", devided[find_queueX][find_queueY]);
+		find_queueY++;
+		if (devided[find_queueX][find_queueY] != '\0')
+		{
+			answer_queue++;
+		}
+		else if (devided[find_queueX][find_queueY] == '\0')
+		{
+			if (answer_queue > answer)
+				answer = answer_queue;
+			answer_queue = 0;
+			printf("%d", answer);
+			find_queueX++;
+			find_queueY = 0;
+			devided[devided_queueX][devided_queueY] = *copied++;		// x,0 공백 제거용
+		}
 	}
-	printf("\n");
 
+	printf("\n");
 	return 0;
 }
 
