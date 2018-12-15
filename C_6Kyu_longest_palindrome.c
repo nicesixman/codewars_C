f685decc186cda84e194ff8d0934f32897dfebaf
nicesixman <nicesixman@naver.com>
2018-12-16 01:10:49 +0900

    C_6Kyu_longest_palindrome.c
    https://www.codewars.com/kata/54bb6f887e5a80180900046b/train/c
    실질적으로 palindrome을 찾기 위해 저장된 공백과 null을 이용하여 단어를 정확하게 끊는 것까지 완료됨.
    단어별로 버블정렬 flow를 이용하면 palindrome을 구현하는 것이 가능할 듯.

--- C_6Kyu_longest_palindrome.c
+++ C_6Kyu_longest_palindrome.c
@@ -9,13 +9,14 @@ int longest_palindrome(const char *s)
 	char devided[1000][1000] = { '\0' };
 	int devided_queueX = 0, devided_queueY = 0;
 	int find_queueX = 0, find_queueY = 0;
+	int for_queueX = 0, for_queueY = 0;
 	int answer_queue = 0, answer = 0;
 
 	printf("-----------------------------------\n");
 	printf("Called longest_palindrome function.\n");
 
 	// C언어는 string을 지원 안하므로 2차원 배열을 이용하여 char별로 저장.
-	// 공백 개수를 포함한 char 개수만큼 for문 반복. 실제로는 공백은 빠진채로 저장됨.
+	// 공백 개수를 포함한 char 개수만큼 for문 반복. 단어의 끝에 공백도 붙는다.
 	for (int slen=0; slen<(int)strlen(s); slen++)
 	{
 		devided[devided_queueX][devided_queueY] = *copied++;
@@ -29,28 +30,35 @@ int longest_palindrome(const char *s)
 	}
 
 	// 실질적으로 palindrome을 찾는 부분.
+	// 단어간 공백을 만나면 다음 2차 행렬로 넘어가고, null을 만나면 프로그램을 종료함.
 	for (int slen=0; slen<(int)strlen(s); slen++)
 	{
 		printf("%c", devided[find_queueX][find_queueY]);
-		find_queueY++;
 		if (devided[find_queueX][find_queueY] != '\0')
 		{
+			find_queueY++;
 			answer_queue++;
+			if (devided[find_queueX][find_queueY] == ' ')
+			{
+				if (answer_queue > answer)
+					answer = answer_queue;
+				answer_queue = 0;
+				find_queueX++;
+				find_queueY = 0;
+				printf("(%d) ", answer);
+			}
+			else if (devided[find_queueX][find_queueY] == '\0')
+			{
+				if (answer_queue > answer)
+					answer = answer_queue;
+				printf("(%d) ", answer);
+				break;
+			}
 		}
-		else if (devided[find_queueX][find_queueY] == '\0')
-		{
-			if (answer_queue > answer)
-				answer = answer_queue;
-			answer_queue = 0;
-			printf("%d", answer);
-			find_queueX++;
-			find_queueY = 0;
-			devided[devided_queueX][devided_queueY] = *copied++;		// x,0 공백 제거용
-		}
+		else
+			return 0;
 	}
-
-	printf("\n");
-	return 0;
+	return answer;
 }
 
 int main()
