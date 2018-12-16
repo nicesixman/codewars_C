63a91bdaf042ed5251c93f8857855c98f7fe6881
nicesixman <nicesixman@naver.com>
2018-12-16 22:03:02 +0900

    C_6Kyu_longest_palindrome.c
    https://www.codewars.com/kata/54bb6f887e5a80180900046b/train/c
    return을 받아갈 수 있도록 수정을 했는데, test 도중 특정 char이 반복되어 나올 때는
    계산을 제대로 해주지 못하고 있는 현상이 발견되어 추가적인 예외처리가 필요하다.

--- C_6Kyu_longest_palindrome.c
+++ C_6Kyu_longest_palindrome.c
@@ -10,11 +10,9 @@ int longest_palindrome(const char *s)
 	int devided_queueX = 0, devided_queueY = 0;
 	int find_queueX = 0, find_queueY = 0;
 	int for_queueY = 1;
-	int answer_queue = 0, answer_Y = 0;
+	int answer_queue = 0, answer_Y = 0, answer = 0, answer_highest = 0;
 
 	printf("-----------------------------------\n");
-	printf("Called longest_palindrome function.\n");
-
 	// C언어는 string을 지원 안하므로 2차원 배열을 이용하여 char별로 저장.
 	// 공백 개수를 포함한 char 개수만큼 for문 반복. 단어의 끝에 공백도 붙는다.
 	for (int slen=0; slen<(int)strlen(s); slen++)
@@ -24,12 +22,12 @@ int longest_palindrome(const char *s)
 		{
 			devided_queueX++;
 			devided_queueY = 0;
-			devided[devided_queueX][devided_queueY] = *copied++;		// x,0 공백 제거용
+			devided[devided_queueX][devided_queueY] = *copied++;		// x,0 공백 제거하며 저장
 		}
 		devided_queueY++;
 	}
 
-	// 실질적으로 palindrome을 찾는 부분.
+	// 실질적으로 palindrome(회문)을 찾는 부분.
 	// 단어간 공백을 만나면 다음 2차 행렬로 넘어가고, null을 만나면 프로그램을 종료함.
 	for (int slen=0; slen<(int)strlen(s); slen++)
 	{
@@ -37,22 +35,36 @@ int longest_palindrome(const char *s)
 		if (devided[find_queueX][find_queueY] != '\0')
 		{
 			find_queueY++;
-			// for_queueY 번째의 char까지 도달하는동안 비교.
+			// for_queueY 번째의 char까지 도달하는동안 모든 문자를 비교.
 			for (for_queueY=0; for_queueY<find_queueY; for_queueY++)
 			{
 				answer_Y++;
+				// for_queueY의 위치에 따라 가변적으로 비교.
+				// (ex. char 7개 단어--> 1번째 char는 1~7 비교, 2번째 char는 2~7 비교, ..., 7번째 char는 7~7 비교.)
 				if (devided[find_queueX][for_queueY] == devided[find_queueX][find_queueY - 1])
 				{
 					answer_queue = 0;
 					answer_queue++;
+					// (char >= 1 이며) palindrome(회문)이 없는 경우에는 answer_highest == 1;
 					if (answer_queue == 1)
-						printf(" (%d %d %d)★%d★\n", for_queueY, find_queueY - 1, answer_queue, answer_Y);
-					if (devided[find_queueX][for_queueY] != devided[find_queueX][answer_Y - 1])
-						printf("HERE IT IS!!!!!\n");
+					{
+						answer = 1;
+						if (answer > answer_highest)
+							answer_highest = answer;
+						// palindrome(회문)이 있는 경우에는 answer_highest를 구함.
+						if (devided[find_queueX][for_queueY] != devided[find_queueX][answer_Y - 1])
+						{
+							// printf(" (%d %d %d)★%d★\n", for_queueY, find_queueY - 1, answer_queue, answer_Y);
+							answer = answer_Y + 1;
+							if (answer > answer_highest)
+								answer_highest = answer;
+							printf("--> palindrome Found!! answer_highest's value: %d\n", answer_highest);
+						}
+					}
+					// ★ 특정 char이 반복되어 나올 때 계산을 제대로 해주지 못하고 있음. 예외처리 필요.
 					answer_Y = 0;
 				}
 			}
-
 			if (devided[find_queueX][find_queueY] == ' ')
 			{
 				find_queueX++;
@@ -64,12 +76,12 @@ int longest_palindrome(const char *s)
 		else
 			return 0;
 	}
-	return 0;
+	return answer_highest;
 }
 
 int main()
 {
-	char *s = "I like racecars that go fast";
+	char *s = "baaaaab";
 	printf("The test string is: I like racecars that go fast\n");
 	longest_palindrome(s);
 
